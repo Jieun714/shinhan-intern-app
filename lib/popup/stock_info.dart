7b4e52dab58dbb386b0d54dd518f5014c106a1a8
Jieun Lee <jieun98.dev@gmail.com>
2023-11-06 00:16:27 +0900

    refactor: data 분리

--- lib/popup/stock_info.dart
+++ lib/popup/stock_info.dart
@@ -1,56 +1,66 @@
 import 'package:flutter/material.dart';
 
 class StockInfo extends StatelessWidget {
-  const StockInfo({super.key});
+  final String choice;
+  const StockInfo(this.choice);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: <Widget>[
-          Row(
-            children: [
-              Flexible(
+    final data;
+    if(choice == 'kosdaq'){
+      data = ['779.28', '8억 1,010만주', '782.53', '768.38'];
+    } else{
+      data = ['2,365.59', '5억 7,866만주', '2,370.28', '2,351.83'];
+    }
+
+    return Padding(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: [
+                Flexible(
                   flex: 2,
                   child: SizedBox(
                     height: 60,
                     child: Align(alignment: Alignment.centerLeft, child: Text('시작'))
                   )
-              ),
-              Flexible(
+                ),
+                Flexible(
                   flex: 2,
                   child: SizedBox(
                     height: 60,
-                    child: Align(alignment: Alignment.centerRight, child: Text('779.28'))
+                    child: Align(alignment: Alignment.centerRight, child: Text(data[0]))
                   )
-              ),
-              Flexible(
+                ),
+                Flexible(
                   flex: 1,
                   child: Container(
                     height: 60,
                   )
-              ),
-              Flexible(
+                ),
+                Flexible(
                   flex: 2,
                   child: SizedBox(
                     height: 60,
                       child: Align(alignment: Alignment.centerLeft, child: Text('거래량'))
                   )
-              ),
-              Flexible(
+                ),
+                Flexible(
                   flex: 2,
                   child: SizedBox(
                     height: 60,
-                      child: Align(alignment: Alignment.centerRight, child: Text('8억 1,010만주'))
+                      child: Align(alignment: Alignment.centerRight, child: Text(data[1]))
                   )
-              )
-            ],
-          ),
-          Divider(
-            thickness: 1,
-            height: 2,
-            color: Color(0xffEFEFEF)
-          ),
-          Row(
+                )
+              ],
+            ),
+            Divider(
+              thickness: 1,
+              height: 2,
+              color: Color(0xffEFEFEF)
+            ),
+           Row(
             children: [
               Flexible(
                   flex: 2,
@@ -63,7 +73,7 @@ class StockInfo extends StatelessWidget {
                   flex: 2,
                   child: SizedBox(
                       height: 60,
-                      child: Align(alignment: Alignment.centerRight, child: Text('782.53'))
+                      child: Align(alignment: Alignment.centerRight, child: Text(data[2]))
                   )
               ),
               Flexible(
@@ -83,7 +93,7 @@ class StockInfo extends StatelessWidget {
                   flex: 2,
                   child: SizedBox(
                       height: 60,
-                      child: Align(alignment: Alignment.centerRight, child: Text('768.38'))
+                      child: Align(alignment: Alignment.centerRight, child: Text(data[3]))
                   )
               )
             ],
@@ -92,6 +102,7 @@ class StockInfo extends StatelessWidget {
               padding: EdgeInsets.only(bottom: 20)
           )
         ],
+      )
     );
   }
 }
