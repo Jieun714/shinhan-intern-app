c647689b18d24ba0597dda4154e0b2c494020363
kkkwp <chxrryda@gmail.com>
2023-11-06 01:07:50 +0900

    feat: debug banner 해제

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '신한 알파',
       theme: ThemeData(
         primaryColor: Colors.white,
